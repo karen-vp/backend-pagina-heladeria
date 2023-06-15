cf36dd5875141d3327b043defb288092c401ecab
karenvp <karenvalierpalmero@gmail.com>
2023-06-14 18:37:32 -0600

    feat: buscar por id de orden compleatado

--- heladeria/src/main/java/com/heladeria/app/repository/ProductHasOrdenRepository.java
+++ heladeria/src/main/java/com/heladeria/app/repository/ProductHasOrdenRepository.java
@@ -1,13 +1,16 @@
 package com.heladeria.app.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 
 import com.heladeria.app.entity.ProductHasOrden;
 
 
 public interface ProductHasOrdenRepository extends JpaRepository<ProductHasOrden, Integer>{
-	Optional<ProductHasOrden> findById(int id);
+	
+	@Query(value="SELECT * FROM productos_has_ordenes WHERE orden_id = ?", nativeQuery=true)
+	List<ProductHasOrden> findByOrderId(int id);
 }
