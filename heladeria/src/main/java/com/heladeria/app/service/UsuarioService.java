528d39713189149ad94165a9b3096ff9d044a021
karenvp <karenvalierpalmero@gmail.com>
2023-06-14 21:15:54 -0600

    feat: actualizacion de funcionalidad

--- heladeria/src/main/java/com/heladeria/app/service/UsuarioService.java
+++ heladeria/src/main/java/com/heladeria/app/service/UsuarioService.java
@@ -5,31 +5,17 @@ import com.heladeria.app.dto.UsuarioDto;
 import com.heladeria.app.entity.Usuario;
 
 public interface UsuarioService {
-	
-// CRUD usando UsuarioDto
-	List<UsuarioDto> getAllUsuarios();
-	
-	UsuarioDto createUsuarioCliente(UsuarioDto usuarioDto);
-	
-	UsuarioDto createUsuarioAdmin(UsuarioDto usuarioDto);
-	
-	UsuarioDto getUsuarioById(int id);
-	
-	Usuario findUsuarioById(int id);
-	
-	void deleteUsuario(int id); 
-	
-	UsuarioDto updateUsuario(UsuarioDto usuarioDto);
+
 	
 //CRUD usando Usuario
 	
-//	List<Usuario> getAllUsuarios();
+	List<Usuario> getAllUsuarios();
 	
-//	Usuario createUsuario(Usuario usuario);
+	Usuario createUsuarioCliente(Usuario usuario);
 	
-//	void deleteUsuario(int id); 
+	void deleteUsuario(int id); 
 	
-//	String updateUsuario (int id, Usuario usuario);
+	String updateUsuario (int id, Usuario usuario);
 	
-	//Usuario getUsuarioById(int id); 
+	Usuario getUsuarioById(int id); 
 }
